f19a11054f86d6d73efa895ee8d3955e0870d3b9
laptop kantor <ryansantoso93@gmail.com>
2025-09-01 12:09:43 +0700

    update gmperp dan buat injek supplier

--- application/modules/parameter/controllers/Supplier.php
+++ application/modules/parameter/controllers/Supplier.php
@@ -756,9 +756,9 @@ class Supplier extends Public_Controller {
     {
         $m_conf = new \Model\Storage\Conf();
         $sql = "
-            select p1.* from pelanggan p1
+            select p1.* from mgb_erp_live.dbo.pelanggan p1
 			right join
-				(select max(id) as id, nomor from pelanggan p where tipe = 'supplier' and jenis <> 'ekspedisi' and mstatus = 1 group by nomor) p2
+				(select max(id) as id, nomor from mgb_erp_live.dbo.pelanggan p where tipe = 'supplier' and jenis <> 'ekspedisi' and mstatus = 1 group by nomor) p2
 				on
 					p1.id = p2.id
 			order by
