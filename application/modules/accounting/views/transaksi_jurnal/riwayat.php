c733b6b074a64f60b7b187de3caf176602c2af84
laptop kantor <ryansantoso93@gmail.com>
2025-09-08 14:20:14 +0700

    update transaksi jurnal

--- application/modules/accounting/views/transaksi_jurnal/riwayat.php
+++ application/modules/accounting/views/transaksi_jurnal/riwayat.php
@@ -68,7 +68,7 @@
 				<?php endforeach ?>
 			<?php else: ?>
 				<tr>
-					<td colspan="3">Data tidak ditemukan.</td>
+					<td colspan="5">Data tidak ditemukan.</td>
 				</tr>
 			<?php endif ?>
 		</tbody>
