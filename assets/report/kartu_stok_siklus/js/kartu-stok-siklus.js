2f2a97ee1638744b425b3ead085425354437d9cd
laptop kantor <ryansantoso93@gmail.com>
2025-10-23 17:02:04 +0700

    upload laporan kartu stok siklus

---
+++ assets/report/kartu_stok_siklus/js/kartu-stok-siklus.js
@@ -0,0 +1,135 @@
+var stok = {
+    startUp: function() {
+        stok.settingUp();
+    }, // end - stok
+
+    setSelect2Plasma: function(elm) {
+        $(elm).select2({
+            ajax: {
+                // delay: 500,
+                // quietMillis: 150,
+                url: 'report/KartuStokSiklus/getPlasma',
+                dataType: 'json',
+                type: 'GET',
+                data: function (params, jenis) {
+                    var query = {
+                        search: params.term,
+                        type: 'item_search',
+						unit: $('select.unit').select2().val(),
+						tutup_siklus: $('select.tutup_siklus').select2().val(),
+                    }
+    
+                    // Query parameters will be ?search=[term]&type=user_search
+                    return query;
+                },
+                processResults: function (data) {
+					// $('li.select2-results__option').attr('aria-selected', false);
+
+                    return {
+                        results: !empty(data) ? data : []
+                    };
+                },
+                error: function (jqXHR, status, error) {
+                    // console.log(error + ": " + jqXHR.responseText);
+                    return { results: [] }; // Return dataset to load after error
+                }
+            },
+            cache: true,
+            placeholder: 'Search for a Plasma ...',
+            // minimumInputLength: 2,
+            escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+            templateResult: function (data) {
+                var markup = "<option value='"+data.id+"'>"+data.text+"</option>";
+                return markup;
+            },
+            templateSelection: function (data, container) {
+                var dataset = null;
+                if ( typeof data.element !== 'undefined' ) {
+                    if ( typeof data.element.dataset !== 'undefined' ) {
+                        dataset = data.element.dataset;
+                    }
+                }
+
+                var tot_cn = !empty(data.tot_cn) ? data.tot_cn : (!empty(dataset) ? dataset.totcn : null);
+
+                $(data.element).attr('data-totcn', data.tot_cn);
+
+                $('.nilai_cn').val(numeral.formatDec(tot_cn));
+
+                return data.text;
+            },
+        });
+    }, // end - setSelect2Plasma
+
+    settingUp: function() {
+        $('select.bulan').select2();
+        $('select.unit').select2();
+        $('select.tutup_siklus').select2();
+        $('select.barang').select2();
+        // $('select.gudang').select2().on("select2:select", function (e) {
+        //     stok.getBarang();
+        // });
+
+        $('#Tahun').datetimepicker({
+            locale: 'id',
+            format: 'Y'
+        });
+
+        // stok.getBarang();
+
+        $(document).ready(function () {
+            stok.setSelect2Plasma( $('.plasma') );
+        });
+    }, // end - stok
+
+    // getBarang: function() {
+    //     var jenis_gudang = $('select.gudang').find('option:selected').attr('data-jenis');
+
+    //     $('select.barang').find('option').removeAttr('disabled');
+    //     $('select.barang').find('option:not([data-jenis="'+jenis_gudang+'"])').attr('disabled', 'disabled');
+    //     $('select.barang').find('option[value="all"]').removeAttr('disabled');
+
+    //     $('select.barang').select2();
+    // }, // end - getData
+
+    getData: function() {
+        var err = 0;
+        $.map( $('[data-required=1]'), function (ipt) {
+            if ( empty( $(ipt).val() ) ) {
+                $(ipt).parent().addClass('has-error');
+                err++;
+            } else {
+                $(ipt).parent().removeClass('has-error');
+            }
+		});
+
+		if ( err > 0 ) {
+			bootbox.alert('Harap lengkapi data terlebih dahulu.');
+		} else {
+            var dcontent = $('table tbody');
+			var params = {
+				'bulan': $('.bulan').select2().val(),
+				'unit': $('.unit').select2().val(),
+				'tutup_siklus': $('.tutup_siklus').select2().val(),
+				'plasma': $('.plasma').select2().val(),
+				'barang': $('.barang').select2().val(),
+				'tahun': dateSQL( $('#Tahun').data('DateTimePicker').date() )
+			};
+
+			$.ajax({
+                url : 'report/KartuStokSiklus/getData',
+                data : {
+                    'params' : params
+                },
+                type : 'GET',
+                dataType : 'HTML',
+                beforeSend : function(){ App.showLoaderInContent( $(dcontent) ); },
+                success : function(html){
+                	App.hideLoaderInContent( $(dcontent), html );
+                }
+            });
+		}
+    }, // end - getData
+};
+
+stok.startUp();
