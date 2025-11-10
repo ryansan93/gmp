24b0a1f9ab3a915d5b6e28d526cbdd85fe3a3156
laptop kantor <ryansantoso93@gmail.com>
2025-11-10 18:33:20 +0700

    update laporan realisasi chickin

--- application/modules/transaksi/controllers/PenerimaanPakan.php
+++ application/modules/transaksi/controllers/PenerimaanPakan.php
@@ -1098,9 +1098,9 @@ class PenerimaanPakan extends Public_Controller {
         //     }
         // }
 
-        $id = '5099';
-        $id_old = '5099';
-        $tanggal = '2025-11-03';
+        $id = '5822';
+        $id_old = '5822';
+        $tanggal = '2025-11-04';
 
         $conf = new \Model\Storage\Conf();
         $sql = "EXEC hitung_stok_siklus 'pakan', 'terima_pakan', '".$id."', '".$tanggal."', 2, null, null";
