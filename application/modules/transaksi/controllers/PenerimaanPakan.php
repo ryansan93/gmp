7a5c295ac7dba0f3014332797647402ccabf8b5a
laptop kantor <ryansantoso93@gmail.com>
2025-11-17 10:52:16 +0700

    update tsdrhpp

--- application/modules/transaksi/controllers/PenerimaanPakan.php
+++ application/modules/transaksi/controllers/PenerimaanPakan.php
@@ -1098,9 +1098,9 @@ class PenerimaanPakan extends Public_Controller {
         //     }
         // }
 
-        $id = '6514';
-        $id_old = '6514';
-        $tanggal = '2025-11-12';
+        $id = '6885';
+        $id_old = '6885';
+        $tanggal = '2025-11-13';
 
         $conf = new \Model\Storage\Conf();
         $sql = "EXEC hitung_stok_siklus 'pakan', 'terima_pakan', '".$id."', '".$tanggal."', 2, null, null";
