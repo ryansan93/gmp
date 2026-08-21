a184b2d47f7fb423322d60ea0c35d17ef8b4a596
laptop kantor <ryansantoso93@gmail.com>
2026-08-21 11:09:26 +0700

    update next number cn

--- application/modules/storage/models/Cn_model.php
+++ application/modules/storage/models/Cn_model.php
@@ -9,7 +9,7 @@ class Cn_model extends Conf {
 
     public function getNextNomor($kode)
 	{
-		$id = $this->whereRaw("SUBSTRING(nomor, LEN('".$kode."')+1, 7) = '/'+cast(right(year(current_timestamp),2) as char(2))+'/'+replace(str(month(getdate()),2),' ',0)+'/'")
+		$id = $this->whereRaw("SUBSTRING(nomor, 1, (LEN('".$kode."')+7)) = '".$kode."'+'/'+cast(right(year(current_timestamp),2) as char(2))+'/'+replace(str(month(getdate()),2),' ',0)+'/'")
                         ->selectRaw("'".$kode."'+'/'+right(year(current_timestamp),2)+'/'+replace(str(month(getdate()),2),' ',0)+'/'+replace(str(substring(coalesce(max(nomor),'000'),((LEN('".$kode."')+1)+(LEN('/'+cast(right(year(current_timestamp),2) as char(2))+'/'+replace(str(month(getdate()),2),' ',0)+'/'))),3)+1,3), ' ', '0') as nextId")
                         ->first();
 		return $id->nextId;
