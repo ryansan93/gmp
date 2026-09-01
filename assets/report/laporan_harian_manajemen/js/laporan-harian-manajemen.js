a09a111c669659a8f2c7bb6e55cd90442b034634
laptop kantor <ryansantoso93@gmail.com>
2026-09-01 16:04:35 +0700

    update Laporan Harian Manajemen

--- assets/report/laporan_harian_manajemen/js/laporan-harian-manajemen.js
+++ assets/report/laporan_harian_manajemen/js/laporan-harian-manajemen.js
@@ -10,6 +10,55 @@ var lhm = {
         });
 	}, // end - settingUp
 
+	copyText: function() {
+        var lines = [];
+        var $data = $('div.data');
+
+        var getText = function($el) {
+            return $el.clone().children().remove().end().text();
+        };
+
+        $data.find('[class*="no-padding"]').each(function() {
+            if ( $(this).hasClass('hide') ) {
+                return true;
+            }
+            if ( $(this).is('#btn-copy-lhm') ) {
+                return true;
+            }
+            var t = getText( $(this) ).replace(/\u00a0/g, ' ');
+            if ( t.trim() === '' ) {
+                lines.push( '' );
+            } else {
+                lines.push( t.trim() );
+            }
+        });
+
+        var text = lines.join('\n');
+
+        if ( navigator.clipboard && navigator.clipboard.writeText ) {
+            navigator.clipboard.writeText( text ).then(function() {
+                bootbox.alert('Data berhasil di-copy.');
+            }, function() {
+                lhm.fallbackCopy( text );
+            });
+        } else {
+            lhm.fallbackCopy( text );
+        }
+    },
+
+    fallbackCopy: function(text) {
+        var $ta = $('<textarea>').val( text ).css({ position: 'fixed', opacity: 0 });
+        $('body').append( $ta );
+        $ta.focus().select();
+        try {
+            document.execCommand('copy');
+            bootbox.alert('Data berhasil di-copy.');
+        } catch (e) {
+            bootbox.alert('Gagal copy data.');
+        }
+        $ta.remove();
+    },
+
 	getLists: function() {
         var err = 0;
         $.map( $('[data-required=1]'), function (ipt) {
